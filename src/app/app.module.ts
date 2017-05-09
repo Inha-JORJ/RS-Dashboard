2de248c2a236dc6f4be407eaaa2d655c1bb6ccb3
JL-Hao <353630611@qq.com>
2017-05-09 13:38:34 +0500

    i am hao

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,19 +6,38 @@ import {HttpModule} from '@angular/http';
 import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import * as Material from '@angular/material';
+import {UserComponent} from './user/user.component';
+import {ProductComponent} from './product/product.component';
+import {CategoryComponent} from './category/category.component';
+import {UserService} from './services/user.service';
+import {NotFoundComponent} from './not-found/not-found.component';
+import {AuthenticationService} from './services/authentication.service';
+import {AlertService} from './services/alert.service';
+import {LoginComponent} from './user/login/login.component';
+import {routing} from "./app.routing";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    ProductComponent,
+    CategoryComponent,
+    NotFoundComponent ,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
     BrowserAnimationsModule,
-    Material.MdSidenavModule
+    routing,
+
+    Material.MdSidenavModule,
+    Material.MdButtonModule,
+    Material.MdInputModule,
+    Material.MdGridListModule,
   ],
-  providers: [],
+  providers: [UserComponent, UserService, AuthenticationService, AlertService],
   bootstrap: [AppComponent]
 })
 export class AppModule {
---
+++ src/app/model/user.model.ts
@@ -0,0 +1,10 @@
+/**
+ * Created by Administrator on 2017/5/6.
+ */
+export class User {
+  id: number;
+  displayname: string;
+  role: string;
+  constructor() {
+  }
+}
