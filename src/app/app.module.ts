3c8a4660fa32299defef3e831dcb5574e09cdadb
Ozodrukh <ozik.dev@gmail.com>
2017-05-09 13:48:44 +0500

    small fixes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,10 @@ import {HttpModule} from '@angular/http';
 import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import * as Material from '@angular/material';
-import { AddproductComponent } from './views/addproduct/addproduct.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AddproductComponent
   ],
   imports: [
     BrowserModule,
